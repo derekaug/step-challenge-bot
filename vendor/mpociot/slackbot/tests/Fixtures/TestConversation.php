164eddf05c78289052b33ead74264e07a64baa1a
Derek J. Augustine <derek.augustine@archrival.com>
2016-12-01 11:47:23 -0600

    committing vendor file because composer isn't working on the server :(

---
+++ vendor/mpociot/slackbot/tests/Fixtures/TestConversation.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Mpociot\SlackBot\Tests\Fixtures;
+
+use Mpociot\SlackBot\Conversation;
+
+class TestConversation extends Conversation
+{
+    /**
+     * @return mixed
+     */
+    public function run()
+    {
+    }
+}
